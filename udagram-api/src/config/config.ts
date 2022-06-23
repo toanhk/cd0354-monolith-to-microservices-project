7c5694ca7dde09596254d19b29aaf0498b3d39f8
ToanHK <toanhk@gmail.com>
2022-06-24 00:36:32 +0700

    Refactor Monolith to Microservices

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -6,7 +6,8 @@ export const config = {
   'dialect': 'postgres',
   'aws_region': process.env.AWS_REGION,
   'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
+   'aws_media_bucket': process.env.AWS_BUCKET,
+  //'aws_media_bucket': 'test-646588003504-dev',
   'url': process.env.URL,
   'jwt': {
     'secret': process.env.JWT_SECRET,
