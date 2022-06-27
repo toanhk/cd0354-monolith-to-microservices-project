2e16bc9e2789ad7c0abac1caac5d7f576ef73a9f
ToanHK <toanhk@gmail.com>
2022-06-27 13:49:49 +0700

    update api

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aff906963fe304789b9f2f266a0cebb1-989848648.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
